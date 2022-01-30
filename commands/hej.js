06f3e5a85c0af859409c8efd8d86524745430370
Unexp1ainable <samorepka314@gmail.com>
2022-01-30 16:04:53 +0100

    init

---
+++ commands/hej.js
@@ -0,0 +1,14 @@
+module.exports = {
+	name: "hej",
+	aliases: [],
+	description: "hou",
+	category: "category",
+	guildOnly: false,
+	memberpermissions: "VIEW_CHANNEL",
+	adminPermOverride: true,
+	cooldown: 2,
+	usage: "<usage>",
+	execute(message) {
+		message.reply("hou");
+	},
+};
